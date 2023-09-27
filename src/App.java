75f6501657ed9e33d3502a8a828047440b6803c7
karielly <kariellyycarvalho@gmail.com>
2023-09-27 20:46:45 -0300

    feat(GerenciadorFinanacas): adicionar menu

--- src/App.java
+++ src/App.java
@@ -1,6 +1,60 @@
-package src;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       System.out.println("Hello, World!");
+        Usuario usuario = null;
+        Scanner input = new Scanner(System.in);
+
+        while (true){
+            System.out.println("Bem-vindo ao sistema de controle financeiro");
+            System.out.println("1- Cadastrar usuário");
+            System.out.println("2- Cadastrar conta");
+            System.out.println("3- Cadastrar receita");
+            System.out.println("4- Cadastrar despesa");
+            System.out.println("5- Cadastrar orçamento");
+            System.out.println("6- Cadastrar meta");
+            System.out.println("7- Cadastrar investimento");
+            System.out.println("0- Sair");
+
+            System.out.println("Digite a opção desejada: ");
+            int opcao = input.nextInt();
+
+            if (opcao == 0) {
+                break;
+            }else if (opcao == 1) {
+                System.out.println("Digite o nome do usuário: ");
+                String nome = input.next();
+                System.out.println("Digite o email do usuário: ");
+                String email = input.next();
+                usuario = new Usuario(nome, email);
+
+            }else if (opcao == 2) {
+                System.out.println("Digite o nome do banco: ");
+                String banco = input.next();
+                System.out.println("Digite a descrição da conta: ");
+                String descricao = input.next();
+
+                Conta conta = new Conta(banco, descricao);
+                usuario.addConta(conta);
+
+            }else if (opcao == 3) {
+                System.out.println("Digite o valor da receita: ");
+                double valor = input.nextDouble();
+                System.out.println("Digite a descrição da receita: ");
+                String descricao = input.next();
+                System.out.println("Digite a data da receita: ");
+                String data = input.next();
+                System.out.println("Informe a categoria da receita: ");
+                String categoria = input.next();
+
+                Receita receita = new Receita(valor, descricao, categoria, data);
+                usuario.addReceita(receita);
+                
+                
+
+        }
+    }
     }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,11 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Usuario {
     private String nome;
     private String email;
+    private List<Conta> contas = new ArrayList<>();
+    private List<Orcamento> orcamentos = new ArrayList<>();
+    private List<Meta> metas = new ArrayList<>();
+    private List<Investimento> investimentos = new ArrayList<>();
+    private List<Receita> receitas = new ArrayList<>();
 
     public Usuario(String nome, String email) {
         this.nome = nome;
         this.email = email;
+
+    }
+
+    public void addReceita(Receita receita) {
+        this.receitas.add(receita);
+    }
+    
+    public void addOrcamento(Orcamento orcamento) {
+        this.orcamentos.add(orcamento);
+    }
+
+    public void addMeta(Meta meta) {
+        this.metas.add(meta);
+    }
+
+    public void addInvestimento(Investimento investimento) {
+        this.investimentos.add(investimento);
+    }
+
+    public void addConta(Conta conta) {
+        this.contas.add(conta);
     }
 
     public String getNome() { 
